9a5192913b75b06b7a069eeb61afd854da4df6e5
aniiyer-host <aniketiyer4@gmail.com>
2026-08-22 15:16:06 +0530

    Restore deployed Trace-It program ID

--- blockchain/programs/traceit/src/lib.rs
+++ blockchain/programs/traceit/src/lib.rs
@@ -6,7 +6,7 @@ pub mod state;
 
 use instructions::*;
 
-declare_id!("9Xvmf151deS4w9evjPprynyFTPDxeDjWRbwruGTY22qa");
+declare_id!("5fj53usXqFvfah3x7rYo6BxQnrvBprBZsGU49XhQxzV3");
 
 #[program]
 pub mod traceit {
